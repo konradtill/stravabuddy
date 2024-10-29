ef8956a54ba3c5ab0e72208df706a72a3257d0ee
Konrad <konrad@MacBook-Pro-Konrad.local>
2024-10-29 19:39:16 +0100

    Initial commit

---
+++ src/main/java/com/halokosmos/stravabuddy/StravabuddyApplication.java
@@ -0,0 +1,13 @@
+package com.halokosmos.stravabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravabuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravabuddyApplication.class, args);
+	}
+
+}
